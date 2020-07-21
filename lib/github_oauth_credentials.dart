d902635a10b494c77bd7c7db01e3976258e71861
OyelowoTobiloba <oluwatobilobaoyelowo@gmail.com>
2020-07-21 16:01:50 +0100

    next step

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,7 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'client ID';
+const githubClientSecret = 'Secret goes here';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
+gs
