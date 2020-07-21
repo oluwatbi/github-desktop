d7feeff7f3c6525794e82015634c9782de462c5d
Nick M <nick.meinhold@gmail.com>
2020-07-22 01:20:42 +1000

    Add macos target
    Co-authored-by: Oyelowo Oluwatobiloba <oluwatobilobaoyelowo@gmail.com>

--- lib/github_oauth_credentials.dart
+++
@@ -1,7 +0,0 @@
-// TODO(CodelabUser): Create an OAuth App
-const githubClientId = 'client ID';
-const githubClientSecret = 'Secret goes here';
-
-// OAuth scopes for repository and user information
-const githubScopes = ['repo', 'read:org'];
-gs
